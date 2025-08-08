eea3894852f40aa95df34fad0e345d219daa369b
Scott Nichols <srnichols@live.com>
2025-08-08 09:24:48 -0600

    dev(local): fix run-local param block and DAB cosmos hostname

--- scripts/run-local.ps1
+++ scripts/run-local.ps1
@@ -1,12 +1,12 @@
 #requires -Version 7.0
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
-
 param(
     [int]$CosmosHostPort = 8085,
     [int]$DabPort = 8082,
     [int]$PortalPort = 8081
 )
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
 
 function Ensure-Docker() {
     if (-not (Get-Command docker -ErrorAction SilentlyContinue)) {
@@ -88,7 +88,7 @@ Wait-Http -url "https://localhost:${CosmosHostPort}/_explorer/emulator.pem" -tim
 $cosmosConnForContainers = 'AccountEndpoint=https://stamps-cosmos:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;'
 $cosmosConnForHost = "AccountEndpoint=https://localhost:${CosmosHostPort}/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;"
 
-Start-Dab -network 'stamps-net' -containerName $dabContainer -dabPort $DabPort -dabConfigPath '.\management-portal\dab\dab-config.json' -cosmosConn $cosmosConnForContainers.Replace('stamps-cosmos','cosmos')
+Start-Dab -network 'stamps-net' -containerName $dabContainer -dabPort $DabPort -dabConfigPath '.\management-portal\dab\dab-config.json' -cosmosConn $cosmosConnForContainers
 
 # Wait for DAB GraphQL
 Wait-Http -url "http://localhost:${DabPort}/graphql" -timeoutSec 120
