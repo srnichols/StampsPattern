0c7fbfeaf79200ab534b5bb07175455bc2b439f1
Scott Nichols <srnichols@live.com>
2025-08-08 00:24:58 -0600

    chore(port): move portal from 8080 to 8081 (Docker + Aspire AppHost)

--- management-portal/AppHost/Program.cs
+++ management-portal/AppHost/Program.cs
@@ -3,7 +3,7 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var portal = builder.AddProject("portal", "..\\src\\Portal\\Portal.csproj")
-    .WithEnvironment("ASPNETCORE_URLS", "http://+:8080")
-    .WithHttpEndpoint(port: 8080, name: "http");
+    .WithEnvironment("ASPNETCORE_URLS", "http://+:8081")
+    .WithHttpEndpoint(port: 8081, name: "http");
 
 builder.Build().Run();
